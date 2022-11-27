49e5e2f6f6db1da0c6c439461f179dffbe6e356c
taekwon8290 <97922491+taekwon8290@users.noreply.github.com>
2022-11-27 21:02:01 +0900

    Update template.py

--- template.py
+++ template.py
@@ -10,56 +10,56 @@ from sklearn.tree import DecisionTreeClassifier, RandomForestClassifier
 from sklearn.svm import SVC
 
 def load_dataset(dataset_path):
-  data_df = pd.read_csv(dataset_path)
-  return data_df
+    data_df = pd.read_csv(dataset_path)
+    return data_df
 	#To-Do: Implement this function
 
 def dataset_stat(dataset_df):	
-  dataset_df.shape[1]
-  n_feats = len(dataset_df.colums)-1
-  #target열을 제외한 나머지 열의 수(feature수)
-  target = list(dataset_df['target'].value_counts())
-  n_class0 = target[0]
-  n_class1 = target[1]
-  #target열에서 0과 1을 분류
-  return n_feats, n_class0, n_class1
+    dataset_df.shape[1]
+    n_feats = len(dataset_df.colums)-1
+    #target열을 제외한 나머지 열의 수(feature수)
+    target = list(dataset_df['target'].value_counts())
+    n_class0 = target[0]
+    n_class1 = target[1]
+    #target열에서 0과 1을 분류
+    return n_feats, n_class0, n_class1
 	#To-Do: Implement this function
 
 def split_dataset(dataset_df, testset_size):
-  x = dataset_df.drop(columns = "target", axis = 1)
-  y = dataset_df["target"]
-  x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = testset_size)
-  return x_train, x_test, y_train, y_test
+    x = dataset_df.drop(columns = "target", axis = 1)
+    y = dataset_df["target"]
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = testset_size)
+    return x_train, x_test, y_train, 
 	#To-Do: Implement this function
 
 def decision_tree_train_test(x_train, x_test, y_train, y_test):
-  dt_cls = DecisionTreeClassifier()
-  dt_cls.fit(x_train, y_train)
+    dt_cls = DecisionTreeClassifier()
+    dt_cls.fit(x_train, y_train)
 
-  accuracy = accuracy_score(y_test, dt_cls.predict(x_test))
-  precision = precision_score(y_test, dt_cls.predict(x_test)) 
-  recall = recall_score(y_test, dt_cls.predict(x_test))
-  return accuracy, precision, recall
+    accuracy = accuracy_score(y_test, dt_cls.predict(x_test))
+    precision = precision_score(y_test, dt_cls.predict(x_test)) 
+    recall = recall_score(y_test, dt_cls.predict(x_test))
+    return accuracy, precision, recall
 	#To-Do: Implement this function
 
 def random_forest_train_test(x_train, x_test, y_train, y_test):
-  rf_cls = RandomForestClassifier()
-  rf_cls.fit(x_train, y_train)
-
-  accuracy = accuracy_score(y_test, rf_cls.predict(x_test))
-  precision = precision_score(y_test, rf_cls.predict(x_test)) 
-  recall = recall_score(y_test, rf_cls.predict(x_test))
-  return accuracy, precision, recall
+    rf_cls = RandomForestClassifier()
+    rf_cls.fit(x_train, y_train)
+    
+    accuracy = accuracy_score(y_test, rf_cls.predict(x_test))
+    precision = precision_score(y_test, rf_cls.predict(x_test)) 
+    recall = recall_score(y_test, rf_cls.predict(x_test))
+    return accuracy, precision, recall
 	#To-Do: Implement this function
 
 def svm_train_test(x_train, x_test, y_train, y_test):
-  svm_cls = SVC()
-  svm_cls.fit(x_train, y_train)
-  
-  accuracy = accuracy_score(y_test, svm_cls.predict(x_test))
-  precision = precision_score(y_test, svm_cls.predict(x_test)) 
-  recall = recall_score(y_test, svm_cls.predict(x_test))
-  return accuracy, precision, recall
+    svm_cls = SVC()
+    svm_cls.fit(x_train, y_train)
+    
+    accuracy = accuracy_score(y_test, svm_cls.predict(x_test))
+    precision = precision_score(y_test, svm_cls.predict(x_test)) 
+    recall = recall_score(y_test, svm_cls.predict(x_test))
+    return accuracy, precision, recall
 	#To-Do: Implement this function
 
 def print_performances(acc, prec, recall):
